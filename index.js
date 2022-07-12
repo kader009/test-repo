e9a85079a10a6cf32a8a42167eb6c97df713c617
kader009 <kadermollarasel000@gmail.com>
2022-07-12 16:11:44 +0600

    last commit

---
+++ index.js
@@ -0,0 +1,67 @@
+//? Operator
+
+
+/* 
+* 1) Arithmatic operator
+
++ addition
+- subtraction
+x multiplication
+/ divition
+
+
+* 2) Remainder operator
+
+% remiander operator
+
+
+* 3) Assignment operator
+
+= assignment
+
+
+* 4) Comparison operator
+
+< less than
+> greater than
+<= less than or equal to
+>= greater than or equal to
+== equal to
+!= not equal 
+
+
+* 5) Logical operator
+
+! not 
+&& and
+|| or
+
+* 6) Exponentiation operator
+
+** exponentiation 
+
+
+*/
+
+
+
+var n = 12;
+var result;
+
+
+var remainder = n % 2;
+
+if(0 == n){
+  result = ' this number is neutral'
+}
+if(0 == remainder){
+  result = 'this is even number'
+}
+else{
+  result = ' this number is odd'
+}
+
+console.log(result);
+
+
+
