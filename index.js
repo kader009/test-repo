b24128c35722e2384ff2a31a11109371e8c4b1e4
kader009 <kadermollarasel000@gmail.com>
2022-07-13 07:29:50 +0600

    hi

--- index.js
+++ index.js
@@ -1,67 +1,15 @@
-//? Operator
-
-
-/* 
-* 1) Arithmatic operator
-
-+ addition
-- subtraction
-x multiplication
-/ divition
-
-
-* 2) Remainder operator
-
-% remiander operator
-
-
-* 3) Assignment operator
-
-= assignment
-
-
-* 4) Comparison operator
-
-< less than
-> greater than
-<= less than or equal to
->= greater than or equal to
-== equal to
-!= not equal 
-
-
-* 5) Logical operator
-
-! not 
-&& and
-|| or
-
-* 6) Exponentiation operator
-
-** exponentiation 
-
-
-*/
 
 
 
-var n = 12;
-var result;
+const fruits = ['banana','orange','apple','mango']
 
 
-var remainder = n % 2;
+fruits.push('lemon')
 
-if(0 == n){
-  result = ' this number is neutral'
-}
-if(0 == remainder){
-  result = 'this is even number'
-}
-else{
-  result = ' this number is odd'
-}
+console.log(fruits);
 
-console.log(result);
+
+
 
 
 
